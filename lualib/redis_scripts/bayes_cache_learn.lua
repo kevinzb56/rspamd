01ef8e0d322dd3e915589427ff0c5e00fef368dd
kevinzb56 <143kevinshah@gmail.com>
2025-04-04 16:47:48 +0530

    modified bayes_cache_learn.lua to support multiple categories

--- lualib/redis_scripts/bayes_cache_learn.lua
+++ lualib/redis_scripts/bayes_cache_learn.lua
@@ -1,22 +1,18 @@
--- Lua script to perform cache checking for bayes classification
--- This script accepts the following parameters:
--- key1 - cache id
--- key3 - is spam (1 or 0)
--- key3 - configuration table in message pack
+-- Multi-Class Bayesian Cache Learning Script
 
 local cache_id = KEYS[1]
-local is_spam = KEYS[2]
+local category = KEYS[2]  -- Multi-class category (finance, personal, etc.)
 local conf = cmsgpack.unpack(KEYS[3])
 cache_id = string.sub(cache_id, 1, conf.cache_elt_len)
 
--- Try each prefix that is in Redis (as some other instance might have set it)
+-- Try each prefix in Redis
 for i = 0, conf.cache_max_keys do
   local prefix = conf.cache_prefix .. string.rep("X", i)
   local have = redis.call('HGET', prefix, cache_id)
 
   if have then
-    -- Already in cache, but is_spam changes when relearning
-    redis.call('HSET', prefix, cache_id, is_spam)
+    -- Already in cache, update category
+    redis.call('HSET', prefix, cache_id, category)
     return false
   end
 end
@@ -29,15 +25,15 @@ for i = 0, conf.cache_max_keys do
     local count = redis.call('HLEN', prefix)
 
     if count < lim then
-      -- We can add it to this prefix
-      redis.call('HSET', prefix, cache_id, is_spam)
+      -- Add to this prefix
+      redis.call('HSET', prefix, cache_id, category)
       added = true
     end
   end
 end
 
 if not added then
-  -- Need to expire some keys
+  -- Expire old keys
   local expired = false
   for i = 0, conf.cache_max_keys do
     local prefix = conf.cache_prefix .. string.rep("X", i)
@@ -46,12 +42,9 @@ if not added then
     if exists then
       if not expired then
         redis.call('DEL', prefix)
-        redis.call('HSET', prefix, cache_id, is_spam)
-
-        -- Do not expire anything else
+        redis.call('HSET', prefix, cache_id, category)
         expired = true
       elseif i > 0 then
-        -- Move key to a shorter prefix, so we will rotate them eventually from lower to upper
         local new_prefix = conf.cache_prefix .. string.rep("X", i - 1)
         redis.call('RENAME', prefix, new_prefix)
       end
